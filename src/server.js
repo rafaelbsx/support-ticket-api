7b93923ae8b3f8fee2ed9903941750b8d044e8a9
RafaelBSX <144088081+rafaelbsx@users.noreply.github.com>
2026-08-30 14:59:35 -0300

    adiciona criação de chamados via POST

--- src/server.js
+++ src/server.js
@@ -13,29 +13,58 @@ const tickets = [
 ];
 
 const server = http.createServer((request, response) => {
-    response.setHeader("content-type", "application/json");
-    
-  if (request.method === "GET" && request.url.startsWith("/tickets/")) {
-    const id = Number(request.url.split("/")[2]);
-    const ticket = tickets.find((ticket) => ticket.id === id);
+  response.setHeader("content-type", "application/json");
 
-  if (!ticket) {
-    response.statusCode = 404;
-    response.end(JSON.stringify({ message: "Ticket not found" }));
+  if (request.method === "GET" && request.url === "/tickets") {
+    response.statusCode = 200;
+    response.end(JSON.stringify({ tickets }));
     return;
   }
 
-  response.statusCode = 200;
-  response.end(JSON.stringify({ ticket }));
-  return;
-}
+  if (request.method === "GET" && request.url.startsWith("/tickets/")) {
+    const id = Number(request.url.split("/")[2]);
+    const ticket = tickets.find((item) => item.id === id);
 
-    response.statusCode = 404;
-    response.end(
-        JSON.stringify({ message: "Route not found" })
-    );
+    if (!ticket) {
+      response.statusCode = 404;
+      response.end(JSON.stringify({ message: "Ticket not found" }));
+      return;
+    }
+
+    response.statusCode = 200;
+    response.end(JSON.stringify({ ticket }));
+    return;
+  }
+
+  if (request.method === "POST" && request.url === "/tickets") {
+    let body = "";
+
+    request.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    request.on("end", () => {
+      const data = JSON.parse(body);
+
+      const ticket = {
+        id: tickets.length + 1,
+        title: data.title,
+        status: "open",
+      };
+
+      tickets.push(ticket);
+
+      response.statusCode = 201;
+      response.end(JSON.stringify({ ticket }));
+    });
+
+    return;
+  }
+
+  response.statusCode = 404;
+  response.end(JSON.stringify({ message: "Route not found" }));
 });
 
 server.listen(3000, () => {
-    console.log("Servidor disponível em http://localhost:3000");
+  console.log("Servidor disponível em http://localhost:3000");
 });
